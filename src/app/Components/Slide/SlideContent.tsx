f31da8a6f9b66c649e547d402cb5e9d706c8ec6b
Rupert Smith <rupert@symbe.co>
2024-10-10 10:15:32 +0100

    Delete things

--- src/app/Components/Slide/SlideContent.tsx
+++ src/app/Components/Slide/SlideContent.tsx
@@ -4,14 +4,9 @@ import { Button } from '../Common/Button';
 type Props = {
   slide: Slide;
   openVariableWindow?: () => void;
-  onClick?: () => void;
 };
 
-export const ExpensiveSlideContent = ({
-  slide,
-  openVariableWindow,
-  onClick,
-}: Props) => {
+export const ExpensiveSlideContent = ({ slide, openVariableWindow }: Props) => {
   // Let's assume this component is expensive to render, and cannot be
   // optimised further - e.g. 20ms render time
   const { title, description } = slide;
@@ -19,10 +14,7 @@ export const ExpensiveSlideContent = ({
   console.log('ExpensiveSlideContent rendered', slide.title);
 
   return (
-    <div
-      className="flex-1 flex flex-col items-center border rounded-2xl p-4 gap-4"
-      onClick={onClick}
-    >
+    <div className="flex-1 flex flex-col items-center border rounded-2xl p-4 gap-4">
       <h1 className="text-2xl font-bold">{title}</h1>
       <p className="text-sm">{description}</p>
       {openVariableWindow && (
